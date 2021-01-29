a22671d168583cb752f60be5ab6a5e1d99c9970b
QSmally <github@qbot.eu>
2021-01-29 19:44:57 +0100

    Prototype the first version of the BackLog cache

---
+++ BackLog.js
@@ -0,0 +1,150 @@
+
+const QDB = require("qdatabase");
+const Qulity = require("qulity");
+
+const Defaults = new Qulity.Collection({
+    "Persistent Path": "BackLog/Persistent.qdb"
+});
+
+class BackLog {
+
+    /**
+     * An off-load key/value-based cache.
+     * @param {String} Identifier 
+     */
+    constructor (Identifier) {
+
+        if (typeof Identifier !== "string") throw new Error("BackLog identifiers should be a type of String.");
+
+        /**
+         * Identifier of this BackLog map.
+         * @name BackLog#Identifier
+         * @type {String}
+         * @readonly
+         */
+        Object.defineProperty(this, "Identifier", {
+            enumerable: true,
+            value: Identifier
+        });
+        
+        /**
+         * Active cache which will get pushed to persistent disk on exit.
+         * @name BackLog#Cache
+         * @type {Map}
+         * @readonly
+         */
+        Object.defineProperty(this, "Cache", {
+            enumerable: true,
+            value: new Qulity.Collection()
+        });
+
+        /**
+         * A defer Promise that resolves once this BackLog is done loading.
+         * @name BackLog#Ready
+         * @type {Promise}
+         * @readonly
+         */
+        Object.defineProperty(this, "Ready", {
+            value: new Promise(Resolve => {
+                setImmediate(() => {
+                    this.PullPersistent();
+                    process.on("beforeExit", this.PushPersistent);
+                    Resolve();
+                });
+            })
+        });
+
+        /**
+         * Settings of this BackLog.
+         * @name BackLog#Settings
+         * @type {Map}
+         * @private
+         */
+        Object.defineProperty(this, "Settings", {
+            value: Defaults.Clone()
+        });
+
+    }
+
+
+    /**
+     * Returns a new QDB Connection.
+     * @returns {Connection}
+     * @private
+     */
+    QDBConnection () {
+        const Path = this.Settings.get("Persistent Path");
+        return new QDB.Connection(Path, {
+            ...this.Settings.get("Connection Options") || {},
+            Table: this.Identifier
+        });
+    }
+
+    /**
+     * Pulls the items from persistent memory into this BackLog.
+     * @returns {BackLog}
+     * @private
+     */
+    PullPersistent () {
+        const Connection = this.QDBConnection();        
+        Connection.Each((V, K) => this.Cache.set(K, V));
+        Connection.Erase(...Connection.Indexes);
+
+        Connection.Disconnect();
+        return this;
+    }
+
+    /**
+     * Moves the current items from this BackLog instance into
+     * persistent memory, provided by the identifier of the BackLog.
+     * @returns {BackLog}
+     * @private
+     */
+    PushPersistent () {
+        const Connection = this.QDBConnection();
+        for (const [Key, Val] of this.Cache)
+        Connection.Set(Key, Val);
+
+        Connection.Disconnect();
+        return this;
+    }
+
+
+    /**
+     * Modifies the settings of this BackLog instance.
+     * @param {Object|*} Key 
+     * @param {*} Val 
+     * @returns {Collection}
+     */
+    Config (Key, Val) {
+        if (typeof Key === "object") {
+            for (const K of Key)
+            this.Settings.set(K, Key[K]);
+            return this.Settings;
+        }
+
+        this.Settings.set(Key, Val);
+        return this.Settings;
+    }
+
+    /**
+     * Modifies the default settings of BackLog.
+     * @param {Object|*} Key 
+     * @param {*} Val 
+     * @returns {Collection}
+     * @static
+     */
+    static Default (Key, Val) {
+        if (typeof Key === "object") {
+            for (const K of Key)
+            Defaults.set(K, Key[K])
+            return Defaults;
+        }
+
+        Defaults.set(Key, Val);
+        return Defaults;
+    }
+
+}
+
+module.exports = BackLog;
